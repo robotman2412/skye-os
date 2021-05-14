d656ff53420d0b23e7c99ec2410b98cbb780b453
RobotMan2412 <julian@scheffers.net>
2021-05-14 13:32:50 +0200

    Update small bits.

--- src/kernel/mkiso.sh
+++ src/kernel/mkiso.sh
@@ -2,19 +2,27 @@
 
 # Download the latest Limine binary release.
 git clone https://github.com/limine-bootloader/limine.git --branch=v2.0-branch-binary --depth=1
- 
+
 # Build limine-install.
 make -C limine
- 
+
 # Create a directory which will be our ISO root.
 mkdir -p iso_root
- 
+
 # Copy the relevant files over.
 cp -v skyekernel.elf limine.cfg limine/limine.sys \
       limine/limine-cd.bin limine/limine-eltorito-efi.bin iso_root/
- 
+
 # Create the bootable ISO.
-xorriso -as mkisofs -b limine-cd.bin \
-        -no-emul-boot -boot-load-size 4 -boot-info-table \
-        -eltorito-alt-boot -e limine-eltorito-efi.bin \
-        -no-emul-boot iso_root -o image.iso
+#cd limine
+#xorriso -as mkisofs -b limine-cd.bin \
+#        -no-emul-boot -boot-load-size 4 -boot-info-table \
+#	-isohybrid-mbr limine-hdd.bin \
+#        -eltorito-alt-boot -e limine-eltorito-efi.bin \
+#        -no-emul-boot ../iso_root/ -isohybrid-gpt-basdat -o ../image.iso
+
+xorriso -as mkisofs -b limine-cd.bin -no-emul-boot -boot-load-size 4 \
+        -boot-info-table -part_like_isohybrid -eltorito-alt-boot \
+        -e limine-eltorito-efi.bin -no-emul-boot ./iso_root/ \
+        -isohybrid-gpt-basdat -o ./image.iso
+./limine/limine-install ./image.iso
